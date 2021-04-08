3c208f94fdb9dbf1e19e85240c3bf9bfc3dd97e3
Víctor M. Reyes <vm.reyesal@gmail.com>
2021-04-08 00:09:50 -0600

    feat: install and config commitlint

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Welcome to repository-template 😎!");
+});
+
+app.listen(port, () => {
+	console.log(`repository-template listening at http://localhost:${port}`);
+});
